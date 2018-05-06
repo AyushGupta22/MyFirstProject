8dab1eb6b6ee5f4f1d96868b1fd9f63c88853506
AyushGupta22 <ayushgupta530@gmail.com>
2018-05-07 00:09:43 +0530

    "New Changes"

--- WebContent/fetch.jsp
+++ WebContent/fetch.jsp
@@ -10,9 +10,11 @@ List<FetchBean> items= Fetch.itemFetch(obj);
 
 FetchBean abc = items.get(0); 
 out.print(abc.getId());
+out.print("<br/><img src=\"images/"+abc.getId()+".jpg\" />");
 for(int i=1;i<items.size();i++){
 	abc = items.get(i);
 	out.print("<br/>"+abc.getId());
+	out.print("<br/><img src=\"images/"+abc.getId()+".jpg\" />");
 }
 %>
   
--- java/main/src/user/Fetch.java
+++ java/main/src/user/Fetch.java
@@ -15,8 +15,8 @@ public class Fetch {
             Class.forName("com.mysql.jdbc.Driver").newInstance();
             con= DriverManager.getConnection("jdbc:mysql:///recommend","root","");
             int id = obj.getId();
-			PreparedStatement ps=con.prepareStatement("select * from item ");
-			//ps.setInt(1,id);
+			PreparedStatement ps=con.prepareStatement("select * from item where item_id = ?");
+			ps.setInt(1,id);
 			ResultSet rs=ps.executeQuery();
             while(rs.next()){
             	int itemId = rs.getInt(1);
@@ -24,15 +24,51 @@ public class Fetch {
             	obj1.setId(itemId);
             	items.add(obj1);
             }
-            /*con.prepareStatement("select sentiment_score_info from item_similarity where item_id = ?) ");
+            int n=0;
+            ps=con.prepareStatement("select count(*) from item");
+			rs=ps.executeQuery();
+            while(rs.next()){
+            	int countItem = rs.getInt(1);
+            	n = countItem;
+            }
+            String arr[] = new String[n];
+            ps = con.prepareStatement("select item_similarity_score_info from item_similarity where item_id = ? ");
 			ps.setInt(1,id);
 			rs=ps.executeQuery();
 			while(rs.next()){
-            	String arr1[] = rs.getString(1).split(",");
+            	arr = rs.getString(1).split(",");
             }
-			con.prepareStatement("select item_sentiment_id,item_sentiment_score from item_sentiment ");
+			ArrayList<Float> arr1 = new ArrayList<Float>();
+			for(int i = 0;i<n;i++){
+				arr1.add(Float.parseFloat(arr[i]));
+			}
+			ArrayList<Float> arr2 = new ArrayList<Float>();
+			ps = con.prepareStatement("select item_sentiment_score from item_sentiment ");
 			rs = ps.executeQuery();
-            */
+			while(rs.next()){
+            	float a = rs.getFloat(1);
+            	arr2.add(a);
+            }
+			
+			for(int i = 0;i < n;i++){
+				if(id != (i+1)){
+					if(i < arr2.size()){
+						if(arr1.get(i) >= 0.5 && arr2.get(i) >= 0.4){
+							FetchBean obj1 = new FetchBean();
+			            	obj1.setId(i+1);
+			            	items.add(obj1);
+						}
+					}
+					else{
+						if(arr1.get(i) >= 0.5){
+							FetchBean obj1 = new FetchBean();
+			            	obj1.setId(i+1);
+			            	items.add(obj1);
+						}
+					}
+				}
+			}
+            
 		}catch(Exception e){
 			System.out.println(e);
 		}
